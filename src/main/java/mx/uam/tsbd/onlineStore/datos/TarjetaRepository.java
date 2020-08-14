2d1f1614781372fee2b25b29a8b451bbfb27b042
ErickESC <eekoston@gmail.com>
2020-08-13 21:04:05 -0500

    Template para registro comenzada

--- src/main/java/mx/uam/tsbd/onlineStore/datos/TarjetaRepository.java
+++
@@ -1,9 +0,0 @@
-package mx.uam.tsbd.onlineStore.datos;
-
-import org.springframework.data.repository.CrudRepository;
-
-import mx.uam.tsbd.onlineStore.negocio.model.Tarjeta;
-
-public interface TarjetaRepository extends CrudRepository<Tarjeta,Integer>{
-
-}
